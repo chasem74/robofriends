7508e32bdada0ec6f90f47373c7a19881d82aef5
Chase Metzger <chasemetzger15@gmail.com>
2018-05-25 14:07:33 -0700

    Added the redux-thunk middleware package

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import {createStore, applyMiddleware} from 'redux';
 import {createLogger} from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
+
 import App from './containers/App';
 import './index.css';
 import 'tachyons';
@@ -10,8 +12,9 @@ import registerServiceWorker from './registerServiceWorker';
 
 import {searchRobots} from './reducers/reducers';
 
-const logger = createLogger();
-const store = createStore(searchRobots);//, applyMiddleware(logger));
+//const logger = createLogger();
+//const store = createStore(searchRobots);//, applyMiddleware(logger));
+const store = createStore(searchRobots, applyMiddleware(thunkMiddleware));
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
